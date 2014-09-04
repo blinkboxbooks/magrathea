824cc41114e96ff0d34f3290a39887f0db5fe125
Anargyros Akrivos <anargyros@blinkbox.com>
2014-09-04 16:42:25 +0100

    Switched to Scala 2.11.2 & Added some DocumentMerger tests

--- build.sbt
+++ build.sbt
@@ -6,27 +6,25 @@ organization := "com.blinkbox.books.marvin"
 
 version := scala.util.Try(scala.io.Source.fromFile("VERSION").mkString.trim).getOrElse("0.0.0")
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.2"
 
 scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8", "-target:jvm-1.7")
 
-javacOptions := Seq("-source", "1.7", "-target", "1.7", "-Xlint:unchecked")
-
 libraryDependencies ++= {
-  val akkaV = "2.3.4"
+  val akkaV = "2.3.5"
   val sprayV = "1.3.1"
   val json4sV = "3.2.10"
   Seq(
-    "io.spray"                  %  "spray-testkit"     % sprayV    % Test,
+    "io.spray"                  %% "spray-testkit"     % sprayV    % Test,
     "org.json4s"                %% "json4s-jackson"    % json4sV,  // for swagger :-/
     "com.typesafe.akka"         %% "akka-slf4j"        % akkaV,
     "com.typesafe.akka"         %% "akka-testkit"      % akkaV     % Test,
-    "com.blinkbox.books"        %% "common-scala-test" % "0.2.1"   % Test,
-    "com.blinkbox.books"        %% "common-spray"      % "0.15.0",
-    "com.blinkbox.books"        %% "common-spray-auth" % "0.5.0",
-    "com.blinkbox.books"        %% "common-config"     % "1.0.1",
-    "com.blinkbox.books"        %% "common-messaging"  % "1.1.1",
-    "com.blinkbox.books.hermes" %% "rabbitmq-ha"       % "6.0.2"
+    "com.blinkbox.books"        %% "common-scala-test" % "0.3.0"   % Test,
+    "com.blinkbox.books"        %% "common-spray"      % "0.16.0",
+    "com.blinkbox.books"        %% "common-spray-auth" % "0.5.1",
+    "com.blinkbox.books"        %% "common-config"     % "1.1.0",
+    "com.blinkbox.books"        %% "common-messaging"  % "1.1.3",
+    "com.blinkbox.books.hermes" %% "rabbitmq-ha"       % "6.0.4"
   )
 }
 
--- src/test/scala/com/blinkbox/books/marvin/magrathea/event/DocumentMergerSpec.scala
+++ src/test/scala/com/blinkbox/books/marvin/magrathea/event/DocumentMergerSpec.scala
@@ -1,12 +1,89 @@
 package com.blinkbox.books.marvin.magrathea.event
 
+import com.blinkbox.books.json.DefaultFormats
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.{DateTime, DateTimeZone}
+import org.json4s.JsonAST.{JNothing, JString, JValue}
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods
 import org.junit.runner.RunWith
 import org.scalatest.FunSuiteLike
 import org.scalatest.junit.JUnitRunner
+import spray.httpx.Json4sJacksonSupport
+
+import scala.language.{implicitConversions, postfixOps}
+import scala.util.Random
 
 @RunWith(classOf[JUnitRunner])
-class DocumentMergerSpec extends FunSuiteLike {
-  test("Test") {
+class DocumentMergerSpec extends FunSuiteLike with Json4sJacksonSupport with JsonMethods {
+  implicit val json4sJacksonFormats = DefaultFormats
+  implicit def dateTime2JValue(d: DateTime) = JString(ISODateTimeFormat.dateTime().print(d.withZone(DateTimeZone.UTC)))
+
+  private def generateId = BigInt(130, Random).toString(16)
+
+  private def sampleBook(extraContent: JValue = JNothing): JValue =
+    ("_id" -> generateId) ~
+    ("$schema" -> "ingestion.book.metadata.v2") ~
+    ("classification" -> List(
+      ("realm" -> "isbn") ~
+      ("id" -> "9780111222333"))
+    ) ~
+    ("source" ->
+      ("$remaining" ->
+        ("system" ->
+          ("name" -> "marvin/design_docs") ~
+          ("version" -> "1.0.0")
+        ) ~
+        ("role" -> "publisher_ftp") ~
+        ("username" -> "jp-publishing") ~
+        ("deliveredAt" -> DateTime.now) ~
+        ("processedAt" -> DateTime.now)
+      )
+    ) merge extraContent
+
+  private def sampleContributor(extraContent: JValue = JNothing): JValue =
+    ("_id" -> generateId) ~
+    ("$schema" -> "ingestion.contributor.metadata.v2") ~
+    ("classification" -> List(
+      ("realm" -> "contributor_id") ~
+      ("id" -> "abc123"))
+    ) ~
+    ("source" ->
+      ("$remaining" ->
+        ("system" ->
+          ("name" -> "marvin/design_docs") ~
+          ("version" -> "1.0.0")
+        ) ~
+        ("role" -> "publisher_ftp") ~
+        ("username" -> "jp-publishing") ~
+        ("deliveredAt" -> DateTime.now) ~
+        ("processedAt" -> DateTime.now)
+      )
+    ) merge extraContent
+
+  test("Must combine two documents so that more recent information is emitted") {
+    val book = sampleBook()
+    println(pretty(render(book)))
+    assert(1 == 1)
+  }
+
+  test("Must not combine two documents with different classifications") {
+    assert(1 == 1)
+  }
+
+  test("Must not replace old data with new data if it is from a less trusted source") {
+    assert(1 == 1)
+  }
+
+  test("Must add sub-objects") {
+    assert(1 == 1)
+  }
+
+  test("Must deep merge sub-objects with different classifications") {
+    assert(1 == 1)
+  }
+
+  test("Must replace an older sub-object with a newer one, if they have the same classification") {
     assert(1 == 1)
   }
 }
