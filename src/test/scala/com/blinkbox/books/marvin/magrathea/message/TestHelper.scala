88bc3984d9c0edde9119b489f62d5f45cd559718
Anargyros Akrivos <anargyros@blinkbox.com>
2014-10-23 16:45:49 +0100

    Implemented some more test helpers

--- src/test/scala/com/blinkbox/books/marvin/magrathea/message/TestHelper.scala
+++ src/test/scala/com/blinkbox/books/marvin/magrathea/message/TestHelper.scala
@@ -1,24 +1,62 @@
 package com.blinkbox.books.marvin.magrathea.message
 
-import org.json4s.JsonAST.{JNothing, JValue}
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.{DateTime, DateTimeZone}
+import org.json4s.JsonAST.{JNothing, JString, JValue}
 import org.json4s.JsonDSL._
 
+import scala.language.implicitConversions
 import scala.util.Random
 
-object Helper {
+trait TestHelper {
+  implicit def dateTime2JValue(d: DateTime) = JString(ISODateTimeFormat.dateTime().print(d.withZone(DateTimeZone.UTC)))
+
+  def generateId = BigInt(130, Random).toString(16)
+
   def getRandomIsbn(num: Int = Random.nextInt(1000000)) = s"9780007${"%06d".format(num)}"
 
-  def sampleBook(extraContent: JValue = JNothing): JValue = {
-    val doc: JValue =
-      ("$schema" -> "ingestion.book.metadata.v2") ~
-      ("classification" -> "something") ~
-      ("source" ->
-        ("system" ->
-          ("name" -> "marvin/design_docs") ~
-          ("version" -> "1.0.0")
-        ) ~
-        ("role" -> "publisher_ftp") ~
-        ("username" -> "jp-publishing"))
-    doc merge extraContent
+  def sampleBook(extraContent: JValue = JNothing, includeId: Boolean = true): JValue = {
+    ("$schema" -> "ingestion.book.metadata.v2") ~
+    ("classification" -> List(
+      ("realm" -> "isbn") ~
+      ("id" -> "9780111222333")
+    )) ~
+    ("source" ->
+      ("system" ->
+        ("name" -> "marvin/design_docs") ~
+        ("version" -> "1.0.0")
+      ) ~
+      ("role" -> "publisher_ftp") ~
+      ("username" -> "jp-publishing") ~
+      ("deliveredAt" -> DateTime.now) ~
+      ("processedAt" -> DateTime.now)
+    ) merge extraContent
+  }
+
+  def sampleContributor(extraContent: JValue = JNothing): JValue = {
+    ("$schema" -> "ingestion.contributor.metadata.v2") ~
+    ("classification" -> List(
+      ("realm" -> "isbn") ~
+      ("id" -> "9780111222333")
+    )) ~
+    ("source" ->
+      ("system" ->
+        ("name" -> "marvin/design_docs") ~
+        ("version" -> "1.0.0")
+      ) ~
+      ("role" -> "publisher_ftp") ~
+      ("username" -> "jp-publishing") ~
+      ("deliveredAt" -> DateTime.now) ~
+      ("processedAt" -> DateTime.now)
+    ) merge extraContent
+  }
+
+  def annotatedSampleBook(extraContent: JValue = JNothing): JValue =
+    DocumentAnnotator.annotate(sampleBook(extraContent))
+
+  def lookupKeyMatch(extraContent: JValue = JNothing): JValue = {
+    ("id" -> "7e93a26396bde6994ccefaf3da003659") ~
+    ("key" -> List("whatever-does-not-matter")) ~
+    ("value" -> ("_id" -> "7e93a26396bde6994ccefaf3da003659") ~ ("_rev" -> "1-da5a08470ffe6bca22174a02f5fd5714"))
   }
 }
