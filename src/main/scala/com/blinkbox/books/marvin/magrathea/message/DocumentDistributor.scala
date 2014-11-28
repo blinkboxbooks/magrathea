8d8c5e01c8a919f2320de618d643556eeb3f3b29
Anargyros Akrivos <anargyros@blinkbox.com>
2014-11-28 17:40:48 +0000

    Added distributor reason and status models

--- src/main/scala/com/blinkbox/books/marvin/magrathea/message/DocumentDistributor.scala
+++ src/main/scala/com/blinkbox/books/marvin/magrathea/message/DocumentDistributor.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.Executors
 
 import com.blinkbox.books.json.DefaultFormats
 import com.blinkbox.books.logging.DiagnosticExecutionContext
+import com.blinkbox.books.marvin.magrathea.message.DocumentDistributor.Reason.Reason
 import com.blinkbox.books.marvin.magrathea.{DistributorConfig, SchemaConfig}
 import com.typesafe.scalalogging.StrictLogging
 import org.json4s.JsonAST.{JString, JValue}
@@ -11,6 +12,15 @@ import org.json4s.jackson.JsonMethods
 import spray.httpx.Json4sJacksonSupport
 
 import scala.concurrent.{ExecutionContext, Future}
+
+object DocumentDistributor {
+  object Reason extends Enumeration {
+    type Reason = Value
+    val NoTitle, Unavailable, Unsuppliable, Unsellable, NoPublisher, NoCover,
+      NoEpub, NotEnglish, NoDescription, NoUsablePrice, Racy = Value
+  }
+  case class Status(sellable: Boolean, reasons: Option[List[Reason]])
+}
 
 class DocumentDistributor(config: DistributorConfig, schemas: SchemaConfig)
   extends Json4sJacksonSupport with JsonMethods with StrictLogging {
---
+++ src/test/scala/com/blinkbox/books/marvin/magrathea/message/DocumentDistributorTest.scala
@@ -0,0 +1,11 @@
+package com.blinkbox.books.marvin.magrathea.message
+
+import com.blinkbox.books.marvin.magrathea.TestHelper
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, FlatSpecLike}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DocumentDistributorTest extends FlatSpecLike with Matchers with TestHelper {
+
+}
